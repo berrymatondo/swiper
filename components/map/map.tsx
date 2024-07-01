57881ee5c6a8aa63b1450edc86674c807e2c510e
jmatondo <j_matondo@hotmail.com>
2024-07-01 07:28:40 +0200

    group cels

--- components/map/map.tsx
+++ components/map/map.tsx
@@ -10,6 +10,8 @@ import "leaflet-defaulticon-compatibility";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { Person } from "@prisma/client";
 import { FaMobileAlt } from "react-icons/fa";
+import MarkerClusterGroup from "react-leaflet-cluster";
+import { Icon } from "leaflet";
 //import { Address, Gi } from "@prisma/client";
 
 type MapProps = {
@@ -28,6 +30,12 @@ export default function Map({ cels, haut }: MapProps) {
 
   let h = haut ? haut : "500px";
 
+  const customIcon = new Icon({
+    //iconUrl: require("../../public/ic.png"),
+    iconUrl: "https://cdn-icons-png.flaticon.com/128/2098/2098567.png",
+    iconSize: [38, 38],
+  });
+
   return (
     <MapContainer
       preferCanvas={true}
@@ -41,36 +49,39 @@ export default function Map({ cels, haut }: MapProps) {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      {cels
-        ?.filter(
-          (adr: any) =>
-            //(secteurId ? gi.secteurId == secteurId : 1 == 1) && gi.address
-            adr?.address?.street
-        )
-        .map((gi: any, index: number) => (
-          <Marker
-            position={[
-              gi.address?.latitude ? +gi.address?.latitude : 0,
-              gi.address?.longitude ? +gi.address?.longitude : 0,
-            ]}
-            key={index}
-          >
-            {" "}
-            <Popup>
-              <p className="text-blue-800 font-semibold">{gi.name}</p>
-              <p>
-                {gi.address.street} {gi.address.number}, {gi.address.postalCode}{" "}
-                {gi.address.municipality}
-              </p>
-              {gi?.persons &&
-                gi?.persons?.map((person: Person) => (
-                  <p className="flex items-center gap-2" key={person.id}>
-                    <FaMobileAlt /> {person.mobile} ({person.firstname})
-                  </p>
-                ))}
-            </Popup>{" "}
-          </Marker>
-        ))}
+      <MarkerClusterGroup>
+        {cels
+          ?.filter(
+            (adr: any) =>
+              //(secteurId ? gi.secteurId == secteurId : 1 == 1) && gi.address
+              adr?.address?.street
+          )
+          .map((gi: any, index: number) => (
+            <Marker
+              position={[
+                gi.address?.latitude ? +gi.address?.latitude : 0,
+                gi.address?.longitude ? +gi.address?.longitude : 0,
+              ]}
+              key={index}
+              icon={customIcon}
+            >
+              {" "}
+              <Popup>
+                <p className="text-blue-800 font-semibold">{gi.name}</p>
+                <p>
+                  {gi.address.street} {gi.address.number},{" "}
+                  {gi.address.postalCode} {gi.address.municipality}
+                </p>
+                {gi?.persons &&
+                  gi?.persons?.map((person: Person) => (
+                    <p className="flex items-center gap-2" key={person.id}>
+                      <FaMobileAlt /> {person.mobile} ({person.firstname})
+                    </p>
+                  ))}
+              </Popup>{" "}
+            </Marker>
+          ))}
+      </MarkerClusterGroup>
 
       {/*       <Marker position={[50.84583, 4.40235]}>
         <Popup>
