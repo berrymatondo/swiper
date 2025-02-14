5074aea1eb2913b5adc44ffa10f6eeafa9888a96
jmatondo <j_matondo@hotmail.com>
2025-02-14 17:58:23 +0100

    fixfix

--- components/appBarr.tsx
+++ components/appBarr.tsx
@@ -4,6 +4,7 @@ import { Button } from "./ui/button";
 import Link from "next/link";
 import MobileNav from "./nav/mobile";
 import NavMob from "./navMob";
+import Image from "next/image";
 
 const AppBar = async () => {
   const session = await auth();
@@ -13,11 +14,11 @@ const AppBar = async () => {
 
   return (
     <div className="z-50 sticky top-0 gap-2 bg-white ">
-      {session && session.user && (
+      {session && session.user ? (
         <div className="ml-auto">
           {session && session.user ? (
             <div>
-              <div className="bg-gray-100 flex justify-between items-center p-2 md:container">
+              <div className="flex justify-between items-center p-2 md:container">
                 <p>
                   <span className="text-sm">Welcome, </span>
                   {usr?.role == "PILOTE" && !usr?.zoneId && (
@@ -39,6 +40,12 @@ const AppBar = async () => {
                       ) : (
                         session?.user?.name
                       )}
+                    </span>
+                  )}
+
+                  {usr?.role == "ADMIN" && (
+                    <span className="text-blue-600 text-sm font-semibold">
+                      {session?.user?.name}
                     </span>
                   )}
                 </p>
@@ -73,6 +80,28 @@ const AppBar = async () => {
           ) : (
             <Link href="/auth/login">Sing IN</Link>
           )}
+        </div>
+      ) : (
+        <div className="flex items-center justify-between px-4 pt-2">
+          <Link href="/" className="flex flex-col items-center">
+            <div className="relative w-10 h-10">
+              <Image
+                src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image-BpN18WLcGdiIVWuat3DeawxVcpar2v.png"
+                alt="ICC Logo"
+                fill
+                className="object-contain"
+              />
+            </div>
+            <span className="text-xs mt-1 text-gray-600">
+              Impact Centre Chrétien
+            </span>
+          </Link>
+          <Link
+            className="text-white bg-gray-400 rounded-full p-2 text-xs"
+            href="/auth/login"
+          >
+            Connexion
+          </Link>
         </div>
       )}
     </div>
